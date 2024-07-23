e0bc296aec2028c4eae9ee319af33ec134724c0d
0xsenzel <shaoqingwong@gmail.com>
2024-07-23 15:19:29 +0800

    removed unnecessary files for boilerplate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { RumpaiModule } from './rumpai/rumpai.module';
 import { AutomapperModule } from '@automapper/nestjs';
 import { classes } from '@automapper/classes';
-import { AuthModule } from './auth/auth.module';
-import { AttachmentController } from './attachment/attachment.controller';
-import { AttachmentService } from './attachment/attachment.service';
-import { AttachmentModule } from './attachment/attachment.module';
-import { SupabaseModule } from './common/configs/supabase';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
@@ -30,15 +24,11 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
       envFilePath: `.env.${process.env.NODE_ENV}.local`,
       isGlobal: true,
     }),
-    RumpaiModule,
     AutomapperModule.forRoot({
       strategyInitializer: classes(),
     }),
-    AuthModule,
-    AttachmentModule,
-    SupabaseModule,
   ],
-  controllers: [AttachmentController],
-  providers: [AttachmentService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/common/configs/supabase/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './supabase.module';
-export * from './supabase.strategy';
-export * from './supabase.guard';
